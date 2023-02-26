babd4e60b40746c939d5ad17e88dd78ae816debe
ldavid94 <ldavid92394@gmail.com>
2023-02-25 18:02:30 -0600

    Modificacion a Prototipo

--- Vista/Registro.Designer.cs
+++ Vista/Registro.Designer.cs
@@ -70,6 +70,8 @@ namespace Vista
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox10
@@ -545,11 +547,31 @@ namespace Vista
             this.label1.TabIndex = 43;
             this.label1.Text = "Información Personal";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 52);
+            this.button1.TabIndex = 84;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(378, 523);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 52);
+            this.button2.TabIndex = 85;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Registro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 526);
+            this.ClientSize = new System.Drawing.Size(769, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.label22);
             this.Controls.Add(this.textBox9);
@@ -641,5 +663,7 @@ namespace Vista
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
